187ee7de1674dbb18b591176011aa5b43d45cfea
PhatNguyen1736 <20521736@gm.uit.edu.vn>
2023-11-08 20:30:31 +0700

    Phat_update

--- Project_Phat/app/src/main/java/com/example/foodorder_project/MainActivity.java
+++ Project_Phat/app/src/main/java/com/example/foodorder_project/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.foodorder_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+//    private TabLayout tabLayout;
+//    private ViewPager2 viewPager2;
+//    private RelativeLayout dangvanchuyen;
+//
+//    @SuppressLint("MissingInflatedId")
+    private RecyclerView rclView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-
-//       // Tạo danh sách thông tin đơn hàng
+        setContentView(R.layout.feedback);
+//        ANH NGUYEN
+       // Tạo danh sách thông tin đơn hàng
 //        ArrayList<String> orderInfoList = new ArrayList<>();
 //        orderInfoList.add("Đơn hàng 2310JK\nĐơn hàng hiện đang trên đường giao đến bạn. Hãy chú ý theo dõi đơn hàng. Nhà hàng Tuệ Duyên xin cảm ơn!");
 //        orderInfoList.add("Đơn hàng 2308JK\nĐơn hàng đã giao thành công. Bạn hãy để lại đánh giá cho món ăn để chúng tôi có thể cải thiện chất lượng món ăn và khách hàng khác có thể hiểu thêm về món ăn nhé!");
@@ -26,6 +38,43 @@ public class MainActivity extends AppCompatActivity {
 //        // Liên kết ArrayAdapter với ListView
 //        ListView listView = findViewById(R.id.infoListView);
 //        listView.setAdapter(adapter);
+
+//         THAO TRANG
+//        tabLayout = findViewById(R.id.tabLayout);
+//        viewPager2 = findViewById(R.id.viewPager);
+//        //dangvanchuyen = findViewById(R.id.frameLayout);
+//
+//        tabLayout.addTab(tabLayout.newTab().setText("Hiện tại"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Lịch sử"));
+//
+//
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        MyFragmentAdapter adapter = new MyFragmentAdapter(fragmentManager, getLifecycle());
+//        viewPager2.setAdapter(adapter);
+//
+//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab) {
+//                viewPager2.setCurrentItem(tab.getPosition());
+//            }
+//
+//            @Override
+//            public void onTabUnselected(TabLayout.Tab tab) {
+//
+//            }
+//
+//            @Override
+//            public void onTabReselected(TabLayout.Tab tab) {
+//
+//            }
+//        });
+//
+//        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+//            @Override
+//            public void onPageSelected(int position) {
+//                tabLayout.selectTab(tabLayout.getTabAt(position));
+//            }
+//        });
 
     }
 }
