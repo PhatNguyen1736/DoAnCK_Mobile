0218e3ec88c29262e1662bbc747219812e0762ce
PhatNguyen1736 <20521736@gm.uit.edu.vn>
2023-11-23 10:19:31 +0700

    New

--- Project_Phat/app/src/main/java/com/example/foodorder_project/MainActivity.java
+++ Project_Phat/app/src/main/java/com/example/foodorder_project/MainActivity.java
@@ -16,30 +16,29 @@ import com.google.android.material.tabs.TabLayout;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-//    private TabLayout tabLayout;
-//    private ViewPager2 viewPager2;
-//    private RelativeLayout dangvanchuyen;
-//
-//    @SuppressLint("MissingInflatedId")
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private RelativeLayout dangvanchuyen;
+
+    @SuppressLint("MissingInflatedId")
     private RecyclerView rclView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.feedback);
+        setContentView(R.layout.menu_chitiet);
 //        ANH NGUYEN
        // Tạo danh sách thông tin đơn hàng
 //        ArrayList<String> orderInfoList = new ArrayList<>();
 //        orderInfoList.add("Đơn hàng 2310JK\nĐơn hàng hiện đang trên đường giao đến bạn. Hãy chú ý theo dõi đơn hàng. Nhà hàng Tuệ Duyên xin cảm ơn!");
 //        orderInfoList.add("Đơn hàng 2308JK\nĐơn hàng đã giao thành công. Bạn hãy để lại đánh giá cho món ăn để chúng tôi có thể cải thiện chất lượng món ăn và khách hàng khác có thể hiểu thêm về món ăn nhé!");
 //
-//        // Tạo ArrayAdapter
+        // Tạo ArrayAdapter
 //        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, orderInfoList);
-//
+
 //        // Liên kết ArrayAdapter với ListView
 //        ListView listView = findViewById(R.id.infoListView);
 //        listView.setAdapter(adapter);
 
-//         THAO TRANG
 //        tabLayout = findViewById(R.id.tabLayout);
 //        viewPager2 = findViewById(R.id.viewPager);
 //        //dangvanchuyen = findViewById(R.id.frameLayout);
